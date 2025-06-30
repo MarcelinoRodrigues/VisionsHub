7049a85334d0951e061316ac687a65251eaf68ef
MarcelinoRodrigues <marcelino.rodrigues.d.n@gmail.com>
2025-06-30 10:22:26 -0300

    first commit

---
+++ VisionsHub/Infra/Data/Context/BaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VisionsHub.Infra.Data.Context
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options) { }
+    }
+}
